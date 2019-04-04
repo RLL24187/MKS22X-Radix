bbb0847f6dc1df17db6cdea28b34b6ecfff6cc2f
rleongPd5 <rleong00@stuy.edu>
2019-04-04 03:58:57 -0400

    Added the splitting nonegatives and negatives

--- Radix.java
+++ Radix.java
@@ -16,6 +16,16 @@ public class Radix{
     }
     String s = "" + max;
     max = s.length();
-
+    //adding to the buckets for the first time
+    for (int i : data){
+      //split data into nonegatives and negatives
+      if (i >= 0){
+        buckets[i%10 + 10].add(i); //second half of the buckets
+      }
+      else{
+        buckets[i%10 + 9].add(i); //first half of the buckets
+      }
+    }
+    MyLinkedList<Integer> m = new MyLinkedList<Integer>();
   }
 }
