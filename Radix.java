81ac4ada2d8c1b728c9e123dd93a073121982bae
rleongPd5 <rleong00@stuy.edu>
2019-04-04 05:01:52 -0400

    Attempted filling buckets loop, didnt test yet

--- Radix.java
+++ Radix.java
@@ -3,7 +3,7 @@ public class Radix{
     @SuppressWarnings("unchecked")
     //creating the buckets
     MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
-    //each bucket will have another linked list inside
+    //each bucket will have another linked list inside, makes this faster
     for (int i = 0; i < 20; i++){
       buckets[i] = new MyLinkedList<Integer>();
     }
@@ -26,6 +26,24 @@ public class Radix{
         buckets[i%10 + 9].add(i); //first half of the buckets
       }
     }
-    MyLinkedList<Integer> m = new MyLinkedList<Integer>();
+    MyLinkedList<Integer> m = new MyLinkedList<Integer>(); //MLL to stores values into
+    int modder = 10; //keep track of what number to mod by for the correct digit
+    while (max > 0){
+      modder *= 10;
+      //fill the buckets in the MLL
+      m.clear(); //make sure the list is clear first
+      for (MyLinkedList<Integer> bucket : buckets){
+        //K said to connect all the linked lists together
+        m.extend(bucket);
+      }
+      for (int i : m){
+        //now for every integer in m (formerly the buckets) fill buckets based on negativity
+        if (i >= 0){
+          buckets[i%modder/(modder/10) + 10].add(i);
+        }
+        else buckets[i%modder/(modder/10)].add(i);
+      }
+      max--; //reduce number of times you still have to sort by buckets
+    }
   }
 }
